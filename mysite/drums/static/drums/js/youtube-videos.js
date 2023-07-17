f01f721f9a46093f95df70b531a5047ee948e516
Dmitriy Skutsenya <dskucenya@okko.tv>
2023-07-17 11:52:03 +0300

    youtube video caver frame

--- mysite/drums/static/drums/js/youtube-videos.js
+++ mysite/drums/static/drums/js/youtube-videos.js
@@ -4,14 +4,18 @@ function loadYoutubeVideos(query) {
     $.get('/videos?query=' + query, function(data) {
         videoContainer.innerHTML = '';
         data.items.forEach(function(item) {
-            const videoFrame = document.createElement('iframe')
-            videoFrame.src = 'https://www.youtube.com/embed/' + item.id.videoId
-            videoFrame.width='480'
-            videoFrame.height='360'
-            videoFrame.frameborder='0'
-            videoFrame.allow='accelerometer; encrypted-media; gyroscope; picture-in-picture; web-share'
-            videoFrame.allowFullscreen=true
-            videoContainer.appendChild(videoFrame) 
+            videoContainer.appendChild(getYouTubeFrame(item.id.videoId)) 
         })
     })
+}
+
+function getYouTubeFrame(youtubeId) {
+    const videoFrame = document.createElement('iframe')
+    videoFrame.src = 'https://www.youtube.com/embed/' + youtubeId
+    videoFrame.width='480'
+    videoFrame.height='360'
+    videoFrame.frameborder='0'
+    videoFrame.allow='accelerometer; encrypted-media; gyroscope; picture-in-picture; web-share'
+    videoFrame.allowFullscreen=true
+    return videoFrame
 }
